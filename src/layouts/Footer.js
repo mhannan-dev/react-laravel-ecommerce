1581312f95bee95648dbe50e8b6c5b39600addb4
Muhammad Hannan <mdhannan.info@gmail.com>
2022-10-28 00:00:11 +0600

    Creater footer, Nav, MasterLayout and Sidebar

---
+++ src/layouts/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+const Footer = () => {
+    return (
+        <footer class="py-4 bg-light mt-auto">
+            <div class="container-fluid px-4">
+                <div class="d-flex align-items-center justify-content-between small">
+                    <div class="text-muted">Copyright © Your Website 2022</div>
+                    <div>
+                        <a href="#">Privacy Policy</a>
+                        <a href="#">Terms &amp; Conditions</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
